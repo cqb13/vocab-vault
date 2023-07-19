d1e2ba60c140a695b6ff03aeac7ded2b6335eed3
cqb13 <cqb13.main@gmail.com>
2023-07-19 12:25:11 -0400

    removed optimizeData script

--- src/data/optimizeData.py
+++
@@ -1,8 +0,0 @@
-import json
-
-with open("latin_addons.json", "r") as f:
-    data = json.load(f)
-    print(data["tickons"])
-
-#with open("latin_addons.json", "w") as f:
-#    json.dump(data, f, separators=(',', ':'))
