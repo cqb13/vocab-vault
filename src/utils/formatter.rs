5193b5b479d355012347846dd91b00b46c9c2aab
cqb13 <cqb13.main@gmail.com>
2023-07-25 13:36:21 -0400

    word info key translation

--- src/utils/formatter.rs
+++ src/utils/formatter.rs
@@ -1,10 +1,12 @@
 use regex::Regex;
 
+use crate::latin_to_english::Word;
 use crate::{Translation, Language};
 use crate::utils::key_translator::translate_part_of_speech;
 use crate::utils::data::Stem;
 
-use super::data::LatinWordInfo;
+use super::data::{LatinWordInfo, WordInfo};
+use super::key_translator::{translate_age, translate_area, translate_frequency, translate_source};
 
 pub fn format_output(mut translation_output: Vec<Translation>, language: Language) -> Vec<Translation> {
 
@@ -25,8 +27,21 @@ fn format_latin_word_info(latin_word_info: LatinWordInfo) -> LatinWordInfo {
     let mut clean_latin_word_info: LatinWordInfo = latin_word_info;
 
     clean_latin_word_info.pos = translate_part_of_speech(&clean_latin_word_info.pos[..]).to_string();
+    clean_latin_word_info.info = format_word_info(clean_latin_word_info.info);
 
     clean_latin_word_info
+}
+
+fn format_word_info(word_info: WordInfo) -> WordInfo {
+    let mut  clean_word_info: WordInfo = word_info;
+
+    clean_word_info.age = translate_age(&clean_word_info.age[..]).to_string();
+    clean_word_info.area = translate_area(&clean_word_info.area[..]).to_string();
+    clean_word_info.geo = translate_area(&clean_word_info.geo[..]).to_string();
+    clean_word_info.freq = translate_frequency(&clean_word_info.freq[..]).to_string();
+    clean_word_info.source = translate_source(&clean_word_info.source[..]).to_string();
+
+    clean_word_info
 }
 
 fn format_latin_stem(latin_stem: Stem) -> Stem {
--- src/utils/key_translator.rs
+++ src/utils/key_translator.rs
@@ -145,3 +145,106 @@ pub fn translate_voice(key: &str) -> &str {
         _ => "unknown",
     }
 }
+
+pub fn translate_age(key: &str) -> &str {
+    match key {
+        "A" => "archaic",
+        "B" => "early",
+        "C" => "classical",
+        "D" => "late",
+        "E" => "later",
+        "F" => "medieval",
+        "G" => "scholar",
+        "H" => "modern",
+        "X" => "used throughout ages",
+        _ => "unknown",
+    }
+}
+
+pub fn translate_area(key: &str) -> &str {
+    match key {
+        "A" => "agriculture",
+        "B" => "biological",
+        "D" => "art",
+        "E" => "religious",
+        "G" => "grammar",
+        "L" => "legal",
+        "P" => "poetic",
+        "S" => "scientific",
+        "T" => "technical",
+        "W" => "warfare",
+        "Y" => "mythological",
+        "X" => "all or none",
+        _ => "unknown",
+    }
+}
+
+pub fn translate_geo(key: &str) -> &str {
+    match key {
+        "A" => "Africa",
+        "B" => "Britain",
+        "C" => "China",
+        "D" => "Scandinavia",
+        "E" => "Egypt",
+        "F" => "France / Gaul",
+        "G" => "Germany",
+        "H" => "Greece",
+        "I" => "Italy / Rome",
+        "J" => "India",
+        "K" => "Balkans",
+        "N" => "Netherlands",
+        "P" => "Persia",
+        "Q" => "Near East",
+        "R" => "Russia",
+        "S" => "Spain / Iberia",
+        "U" => "Eastern Europe",
+        "X" => "all or none",
+        _ => "unknown",
+    }
+}
+
+pub fn translate_frequency(key: &str) -> &str {
+    match key {
+        "A" => "very frequent",
+        "B" => "frequent",
+        "C" => "common",
+        "D" => "lesser",
+        "E" => "uncommon",
+        "F" => "very rare",
+        "I" => "inscription",
+        "M" => "graffiti",
+        "N" => "Pliny (only in Pliny Natural History)",
+        "X" => "all or none",
+        _ => "unknown",
+    }
+}
+
+pub fn translate_source(key: &str) -> &str {
+    match key {
+        "B" => "C.H.Beeson, A Primer of Medieval Latin, 1925 (Bee)",
+        "C" => "Charles Beard, Cassell's Latin Dictionary 1892 (CAS)",
+        "D" => "J.N.Adams, Latin Sexual Vocabulary, 1982 (Sex)",
+        "E" => "L.F.Stelten, Dictionary of Eccles. Latin, 1995 (Ecc)",
+        "F" => "Roy J. Deferrari, Dictionary of St. Thomas Aquinas, 1960 (DeF)",
+        "G" => "Gildersleeve + Lodge, Latin Grammar 1895 (G+L)",
+        "H" => "Collatinus Dictionary by Yves Ouvrard",
+        "I" => "Leverett, F.P., Lexicon of the Latin Language, Boston 1845",
+        "K" => "Calepinus Novus, modern Latin, by Guy Licoppe (Cal)",
+        "L" => "Lewis, C.S., Elementary Latin Dictionary 1891",
+        "M" => "Latham, Revised Medieval Word List, 1980",
+        "N" => "Lynn Nelson, Wordlist",
+        "O" => "Oxford Latin Dictionary, 1982 (OLD)",
+        "P" => "Souter, A Glossary of Later Latin to 600 A.D., Oxford 1949",
+        "Q" => "Other, cited or unspecified dictionaries",
+        "R" => "Plater & White, A Grammar of the Vulgate, Oxford 1926",
+        "S" => "Lewis and Short, A Latin Dictionary, 1879 (L+S)",
+        "T" => "Found in a translation  --  no dictionary reference",
+        "U" => "Du Cange",
+        "V" => "Vademecum in opus Saxonis - Franz Blatt (Saxo)",
+        "W" => "My personal guess",
+        "Y" => "Temp special code",
+        "Z" => "Sent by user --  no dictionary reference",
+        "X" => "general",
+        _ => "unknown",
+    }
+}
