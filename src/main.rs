4b3d616dd0b45219b01910bf5b5e1263c7833079
cqb13 <cqb13.main@gmail.com>
2024-02-15 13:35:12 -0500

    started new command

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 pub mod cli;
 pub mod dictionary_structures;
 pub mod translators;
+pub mod use_data;
 pub mod utils;
 
 use cli::{Arg, Cli, Command, Property};
 use translators::english_to_latin::translate_english_to_latin;
 use translators::latin_to_english::translate_latin_to_english;
 use translators::{DisplayType, Language, Translation, TranslationType};
+use use_data::WordType;
 use utils::sanitize_word;
 
 fn main() {
-    let global_args = vec![
+    let global_args_for_translation = vec![
         Arg::new()
             .with_name("words")
             .with_value_name("WORDS")
@@ -47,15 +49,80 @@ fn main() {
         Property::Auto,
         Property::Auto,
         vec![
-            Command::new("transEng", "Translate english to latin").with_args(&global_args),
+            Command::new("transEng", "Translate english to latin")
+                .with_args(&global_args_for_translation),
             Command::new("transLat", "Translate latin to english")
-                .with_args(&global_args)
+                .with_args(&global_args_for_translation)
                 .with_arg(
                     Arg::new()
                         .with_name("tricks")
                         .with_short('t')
                         .with_long("tricks")
                         .with_help("Will attempt to use various tricks to find the translation"),
+                ),
+            Command::new("getList", "Gets a list of words based on the options provided")
+                .with_arg(
+                    Arg::new()
+                    .with_name("type")
+                    .with_value_name("TYPE")
+                    .with_help("The type of words to get. Options: english, latin, inflections, not-packons, packons, prefixes, stems, suffixes, tackons, tickons, unique-latin"),
+                )
+                .with_arg(
+                    Arg::new()
+                    .with_name("pos")
+                    .with_short('p')
+                    .with_long("pos")
+                    .with_value_name("POS")
+                    .with_help("The part of speeches to include, separated by commas"),
+                )
+                .with_arg(
+                    Arg::new()
+                    .with_name("max")
+                    .with_short('m')
+                    .with_long("max")
+                    .with_value_name("MAX")
+                    .with_help("The maximum word length"),
+                )
+                .with_arg(
+                    Arg::new()
+                    .with_name("min")
+                    .with_short('n')
+                    .with_long("min")
+                    .with_value_name("MIN")
+                    .with_help("The minimum word length"),
+                )
+                .with_arg(
+                    Arg::new()
+                    .with_name("exact")
+                    .with_short('e')
+                    .with_long("exact")
+                    .with_value_name("EXACT")
+                    .with_help("The exact word length"),
+                )
+                .with_arg(
+                    Arg::new()
+                    .with_name("amount")
+                    .with_short('a')
+                    .with_long("amount")
+                    .with_value_name("AMOUNT")
+                    .with_help("The amount of words to get"),
+                )
+                .with_arg(
+                    Arg::new()
+                    .with_name("random")
+                    .with_short('r')
+                    .with_long("random")
+                    .with_value_name("RANDOM")
+                    .with_help("Get words from a random position")
+                    .requires("amount"),
+                )
+                .with_arg(
+                    Arg::new()
+                    .with_name("to")
+                    .with_short('t')
+                    .with_long("to")
+                    .with_value_name("TO")
+                    .with_help("The file to export the results to"),
                 ),
             Command::new("help", "Helps you"),
         ],
@@ -90,6 +157,42 @@ fn main() {
             let tricks = command.has("tricks");
 
             latin_to_english(&words, max, tricks, sort, pretty, detailed);
+        }
+        "getList" => {
+            let type_of_words = command.get_value().throw_if_none();
+            let pos = command.get_value_of("pos");
+            let max = command.get_value_of("max");
+            let min = command.get_value_of("min");
+            let exact = command.get_value_of("exact");
+            let amount = command.get_value_of("amount");
+            let random = command.has("random");
+            let to = command.get_value_of("to");
+
+            if type_of_words != "english"
+                && type_of_words != "latin"
+                && type_of_words != "inflections"
+                && type_of_words != "not-packons"
+                && type_of_words != "packons"
+                && type_of_words != "prefixes"
+                && type_of_words != "stems"
+                && type_of_words != "suffixes"
+                && type_of_words != "tackons"
+                && type_of_words != "tickons"
+                && type_of_words != "unique-latin"
+            {
+                println!(
+                    "Invalid type of words. Please use `help` to see the available types of words."
+                );
+                return;
+            }
+
+            let word_type = WordType::from_str(type_of_words.as_str())
+                .unwrap_or_else(|e| panic!("Error: {}", e));
+
+            println!(
+                "type: {}, pos: {:?}, max: {:?}, min: {:?}, exact: {:?}, amount: {:?}, random: {:?}, to: {:?}",
+                type_of_words, pos, max, min, exact, amount, random, to
+            );
         }
         "help" => {
             cli.help();
---
+++ src/use_data/mod.rs
@@ -0,0 +1,32 @@
+pub enum WordType {
+    English,
+    Latin,
+    Inflections,
+    NotPackons,
+    Packons,
+    Prefixes,
+    Stems,
+    Suffixes,
+    Tackons,
+    Tickons,
+    UniqueLatin,
+}
+
+impl WordType {
+    pub fn from_str(s: &str) -> Result<WordType, String> {
+        match s {
+            "english" => Ok(WordType::English),
+            "latin" => Ok(WordType::Latin),
+            "inflections" => Ok(WordType::Inflections),
+            "not_packons" => Ok(WordType::NotPackons),
+            "packons" => Ok(WordType::Packons),
+            "prefixes" => Ok(WordType::Prefixes),
+            "stems" => Ok(WordType::Stems),
+            "suffixes" => Ok(WordType::Suffixes),
+            "tackons" => Ok(WordType::Tackons),
+            "tickons" => Ok(WordType::Tickons),
+            "unique_latin" => Ok(WordType::UniqueLatin),
+            _ => Err(format!("Invalid word type: {}", s)),
+        }
+    }
+}
